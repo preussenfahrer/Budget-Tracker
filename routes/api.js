c8b589948d985af04059930b9183479e60253b13
Pere Grinus <gregjleighton@gmail.com>
2021-01-31 14:59:01 -0500

    added service worker in index.html, still finding other problems with web manifest.

--- routes/api.js
+++ routes/api.js
@@ -31,14 +31,5 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
-router.get("/api/images", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
 
 module.exports = router;
