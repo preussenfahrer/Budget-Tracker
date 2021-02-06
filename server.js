37b1bc5649c6f4b98469636fc0cb5baed6f152dc
Pere Grinus <gregjleighton@gmail.com>
2021-02-06 12:49:17 -0500

    found potential error in server.js, where I had used useFindAndModify: false two times, and did not use useUnifiedToplogy: true. Deleted one instance and replace with the proper one. Will now see if this is responsible for the deployment error. Also noticed that process.env.port was not process.env.PORT. changed this.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
